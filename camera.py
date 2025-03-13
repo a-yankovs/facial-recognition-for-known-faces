559ed4c2812e8c7adf567c604c3781b4a020dffa
Alexandra <150750157+a-yankovs@users.noreply.github.com>
2025-03-12 23:00:36 -0700

    Update camera.py to include inline comments

--- camera.py
+++ camera.py
@@ -1,22 +1,25 @@
-
 import cv2 
-    
+
+# function which initialises camera and sets its resolution and fps
+# returns the new camera object 
 def camera_init():
     cam = cv2.VideoCapture(0)
     if not cam.isOpened():
         print("Error: Could not initialize camera.")
         return None
     print("Camera initialized successfully.")
-    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)  # Reduce resolution to speed up
+    # reduced resolution to help speed up program 
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640) 
     cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-     # Set FPS to a higher value (e.g., 30 FPS)
-    cam.set(cv2.CAP_PROP_FPS, 30)  # Or another camera index if needed  # Debugging line
+    cam.set(cv2.CAP_PROP_FPS, 30)
     return cam
 
-# ret - boolean - whether the frame was captured successfully 
-# frame - the camptured frame itself - npy array 
+# contiuously captures the current frame of the camera  
+# returns the captured frame 
 def capture_frame(cam):
+    # ret - boolean - whether the frame was captured successfully 
+    # frame - the camptured frame itself - npy array 
     ret, frame = cam.read()
     if not ret:
         print("Error: Unable to capture frame.")
